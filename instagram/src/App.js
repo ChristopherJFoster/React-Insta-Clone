ef2d99c06e1b0d51e156025d79b4ad88d57cd203
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-20 17:17:58 -0500

    MVP: authenticate.js HOC works as described in the instructions.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,21 +1,7 @@
-import React from "react";
 import "./css/main.css";
 import authenticate from "./authenticate/authenticate";
-import PostsPage from "./components/PostContainer/PostsPage";
 import Login from "./components/Login/Login";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-  render() {
-    console.log("App render");
-    return localStorage.getItem("username") ? <PostsPage /> : <Login />;
-  }
-}
+const App = () => {};
 
-// I think this is right...:
-App = authenticate(App);
-
-export default App;
+export default authenticate(App)(Login);
--- instagram/src/authenticate/authenticate.js
+++ instagram/src/authenticate/authenticate.js
@@ -1,11 +1,24 @@
 import React from "react";
+import PostsPage from "../components/PostContainer/PostsPage";
 
-const authenticate = App =>
-  class extends React.Component {
+const authenticate = App => Login => {
+  return class extends React.Component {
+    constructor() {
+      super();
+      this.state = { loggedIn: false };
+    }
+
+    componentDidMount() {
+      if (localStorage.getItem("username")) {
+        this.setState({ loggedIn: true });
+      }
+    }
+
     render() {
       console.log("authenticate render");
-      return <App />;
+      return this.state.loggedIn ? <PostsPage /> : <Login />;
     }
   };
+};
 
 export default authenticate;
