831cc4a3541a78024d440db4e5d6a30f784c0154
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-20 19:50:07 -0500

    Stretch: A new comment is attributed to the username in localStorage (implemented previously). Styled Login.js.

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,13 +1,16 @@
 import React from "react";
-// import igLogo from "./igLogo.svg";
-import igWordmark from "./igWordmark.png";
+import igWordmark from "../../assets/igWordmark.png";
 
 const SearchBar = ({ clearLocalStorage, changeHandler, searchText }) => {
   return (
     <div className="search-bar">
       <div className="logo-set">
         <i onClick={clearLocalStorage} className="logo fab fa-instagram" />
-        <img src={igWordmark} alt="instagram wordmark" className="wordmark" />
+        <img
+          src={igWordmark}
+          alt="instagram wordmark"
+          className="wordmark-search-bar"
+        />
       </div>
       <input
         name="searchText"
